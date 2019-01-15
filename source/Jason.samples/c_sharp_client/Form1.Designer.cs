086bf622543b39c530bba5f92281d528f9493c75
NotoOotori <345238563@qq.com>
2019-01-15 19:20:37 +0900

    test: 创建了C#客户端的样例
    C#客户端样例, 使用方法是点击connect按钮, 之后可在文本框中输入信息, 点击send或者按回车键发送信息给服务器, 从服务器接收到的信息会显示在下方的label中.

---
+++ source/Jason.samples/c_sharp_client/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace c_sharp_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_connect = new System.Windows.Forms.Button();
+            this.textBox_send = new System.Windows.Forms.TextBox();
+            this.label_response = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(148, 71);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 0;
+            this.button_connect.Text = "connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // textBox_send
+            // 
+            this.textBox_send.Location = new System.Drawing.Point(71, 139);
+            this.textBox_send.Name = "textBox_send";
+            this.textBox_send.Size = new System.Drawing.Size(100, 25);
+            this.textBox_send.TabIndex = 2;
+            this.textBox_send.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_send_KeyDown);
+            // 
+            // label_response
+            // 
+            this.label_response.AutoSize = true;
+            this.label_response.Location = new System.Drawing.Point(27, 211);
+            this.label_response.Name = "label_response";
+            this.label_response.Size = new System.Drawing.Size(39, 15);
+            this.label_response.TabIndex = 3;
+            this.label_response.Text = "NULL";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(221, 138);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(97, 23);
+            this.button_send.TabIndex = 4;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 326);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label_response);
+            this.Controls.Add(this.textBox_send);
+            this.Controls.Add(this.button_connect);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.TextBox textBox_send;
+        private System.Windows.Forms.Label label_response;
+        private System.Windows.Forms.Button button_send;
+    }
+}
+
