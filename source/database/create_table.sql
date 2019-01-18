4584150bedc17c53a88c8a3af8cbff8c50cc0eb9
NotoOotori <345238563@qq.com>
2019-01-18 13:55:23 +0900

    refactor(database/create_table): 修改注释, 增加约束
    删除了不必要的注释, 修改了错误的注释, 添加了server_log.login_time的NOT NULL约束

--- source/database/create_table.sql
+++ source/database/create_table.sql
@@ -23,18 +23,15 @@ CREATE TABLE platform_user
 CREATE TABLE server_log
 (
 	email_address VARCHAR(254),
-    # CHECK (email_address LIKE '%@%'),
     server_host CHAR(15),
-    # CHECK (server_host >= '000.000.000.000' AND server_host <= '255.255.255.255'),
     server_port SMALLINT(4),
-    # CHECK (server_port >= 0 AND server_port <= 65535),
-	login_time DATETIME,
+	login_time DATETIME NOT NULL,
     logout_time DATETIME,
-    # login_time <= logout_time
+    # login_time <= logout_time,
     user_host CHAR(15),
     # CHECK (user_host >= '000.000.000.000' AND user_host <= '255.255.255.255'),
     user_port SMALLINT(4),
-    # CHECK (user >= 0 AND user <= 65535),
+    # CHECK (user_port >= 0 AND user_port <= 65535),
     PRIMARY KEY (email_address, server_host, server_port, login_time),
 	FOREIGN KEY (email_address)
 		REFERENCES platform_user (email_address)
