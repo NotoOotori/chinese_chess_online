b45526d9d9a3a086086dd7d3727b69a9a6e580f9
NotoOotori <345238563@qq.com>
2019-01-18 21:26:36 +0900

    feat(database): 创建注册存储过程
    该存储过程中, 我们用salted SHA256来加密密码
    
    #15

---
+++ source/database/procedure_sign_up.sql
@@ -0,0 +1,22 @@
+DELIMITER //
+
+CREATE PROCEDURE procedure_sign_up
+(
+	IN _email_address VARCHAR(254),
+    IN _username VARCHAR(16),
+    IN _unencrypted_password VARCHAR(256),
+    IN _gender CHAR(1),
+    IN _birthday DATE
+)
+BEGIN
+	SET @_salt_value = RANDOM_BYTES(64);
+    SET @_encrypted_password = SHA2(CONCAT(
+		@_salt_value, _unencrypted_password), 256);
+    INSERT INTO platform_user VALUE
+    (
+		_email_address, _username, @_encrypted_password,
+        @_salt_value, _gender, _birthday
+	);
+END //
+
+DELIMITER ;
