2fe222f38160c8159afea28c7a7114e4b0de0196
NotoOotori <345238563@qq.com>
2019-01-19 02:03:21 +0900

    style(database): 删除注释中无意义的逗号

--- source/database/create_table.sql
+++ source/database/create_table.sql
@@ -1,23 +1,23 @@
 CREATE TABLE platform_server
 (
     server_hostname CHAR(15),
-    # CHECK (server_hostname >= '000.000.000.000' AND server_hostname <= '255.255.255.255'),
+    # CHECK (server_hostname >= '000.000.000.000' AND server_hostname <= '255.255.255.255')
     server_port SMALLINT(4),
-    # CHECK (server_port >= 0 AND server_port <= 65535),
+    # CHECK (server_port >= 0 AND server_port <= 65535)
     PRIMARY KEY (server_hostname, server_port)
 );
 
 CREATE TABLE platform_user
 (
     email_address VARCHAR(254) PRIMARY KEY,
-    # CHECK (email_address LIKE '%@%'),
+    # CHECK (email_address LIKE '%@%')
     username VARCHAR(16) NOT NULL,
     encrypted_password BINARY(64) NOT NULL,
     salt_value BINARY(64) NOT NULL,
     gender CHAR(1),
-    # CHECK (gender LIKE '[fm]'),
+    # CHECK (gender LIKE '[fm]')
     birthday DATE
-    # CHECK (birthday <= CURDATE()),
+    # CHECK (birthday <= CURDATE())
     # state: store whether is blocked?
 );
 
@@ -30,9 +30,9 @@ CREATE TABLE server_log
     logout_time DATETIME,
     # login_time <= logout_time,
     user_host CHAR(15),
-    # CHECK (user_host >= '000.000.000.000' AND user_host <= '255.255.255.255'),
+    # CHECK (user_hostname BETWEEN '000.000.000.000' AND '255.255.255.255')
     user_port SMALLINT(4),
-    # CHECK (user_port >= 0 AND user_port <= 65535),
+    # CHECK (user_port >= 0 AND user_port <= 65535)
     PRIMARY KEY (email_address, server_hostname, server_port, login_time),
     FOREIGN KEY (email_address)
         REFERENCES platform_user (email_address)
