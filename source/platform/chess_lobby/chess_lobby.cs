4430adc2724903e03fb5adf7bae48be753ddb925
NotoOotori <345238563@qq.com>
2019-01-20 01:32:53 +0900

    feat(platform.chess_lobby): 新建chess_lobby类

---
+++ source/platform/chess_lobby/chess_lobby.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace platform.chess_lobby
+{
+    public partial class chess_lobby : Form
+    {
+        public chess_lobby()
+        {
+            InitializeComponent();
+        }
+    }
+}
