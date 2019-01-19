c64c4837b78d38b00019575835fb7ccc2b89fde6
NotoOotori <345238563@qq.com>
2019-01-19 19:25:56 +0900

    test(database): 添加注册和登录存储过程的测试用例

---
+++ source/database/test.sql
@@ -0,0 +1,16 @@
+INSERT INTO platform_server
+    VALUE ('45.32.82.133', 21567);
+
+DELETE FROM platform_server
+    WHERE hostname = '45.32.82.133';
+
+CALL procedure_sign_up('345238563@qq.com', 'noto_ootori', '123pzy', 'f', '1999-04-01');
+
+DELETE FROM platform_user
+    WHERE username = 'noto_ootori';
+
+CALL procedure_log_in('345238563@qq.com', '123pzy1', '45.32.82.133', 21567, '111.187.30.131', 49137, @_status_code, @_login_id);
+SELECT @_status_code, @_login_id;
+
+DELETE FROM user_login_record WHERE email_address = '345238563@qq.com';
+ALTER TABLE user_login_record AUTO_INCREMENT = 1;
