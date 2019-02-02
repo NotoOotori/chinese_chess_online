271c0b8a441b0b528306a4b7c764b1081896fa82
NotoOotori <345238563@qq.com>
2019-02-03 01:06:22 +0900

    feat(source/server): 测试版server
    还需要做字符串的处理, 重复登录的处理
    
    #24

---
+++ source/server/server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace server
+{
+    public class Server
+    {
+        #region ' Constructors '
+
+        public Server()
+        {
+            server_socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp)
+            {
+                ReceiveBufferSize = BUFSIZ,
+                SendBufferSize = BUFSIZ
+            };
+            server_socket.Bind(end_point);
+            server_socket.Listen(20);
+        }
+
+        #endregion
+
+        #region 'Fields and Properties '
+
+        private const String HOST = "45.32.82.133";
+        private const Int32 PORT = 21567;
+        private IPEndPoint end_point = new IPEndPoint(
+            IPAddress.Parse(HOST), PORT);
+        private const Int32 BUFSIZ = 1024 * 1024;
+
+        private Socket server_socket { get; set; }
+
+        #endregion
+
+        #region ' Methods '
+
+        public void mainloop()
+        {
+            while (true)
+            {
+                Socket client_socket = server_socket.Accept();
+                ParameterizedThreadStart pts = 
+                    new ParameterizedThreadStart(client_thread);
+                Thread thread = new Thread(pts);
+                thread.Start(client_socket);
+            }
+        }
+
+        private void client_thread(object client_socket_para)
+        {
+            Socket client_socket = client_socket_para as Socket;
+            User user = new User(client_socket);
+            String ep = client_socket.RemoteEndPoint.ToString();
+            try
+            {
+                while (true)
+                {
+                    Byte[] arr_data = new Byte[BUFSIZ];
+                    Int32 length = client_socket.Receive(arr_data);
+                    String str_data = Encoding.UTF8.GetString(
+                        arr_data, 0, length);
+                    if (str_data == null)
+                    {
+                        Console.WriteLine($"{ep} disconnected.");
+                        break;
+                    }
+                    Int32 code = user.log_in("345238563@qq.com", "123pzy");
+                    client_socket.Send(Encoding.UTF8.GetBytes(code.ToString()));
+                }
+            }
+            catch(SocketException)
+            {
+                client_socket.Close();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ source/server/user.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.Sockets;
+using MySql.Data.MySqlClient;
+
+namespace server
+{
+    public class User
+    {
+        #region ' Constructors '
+
+        public User(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        #endregion
+
+        #region ' Properties '
+
+        private String CONNECTION_STRING =
+            "server = localhost; " +
+            "user = ccol_user; " +
+            "database = chinese_chess_online; " +
+            "port = 3306; " +
+            "password = 123PengZiYu@";
+
+        private Socket socket { get; set; }
+        public IPEndPoint client_end_point
+        {
+            get { return socket.RemoteEndPoint as IPEndPoint; }
+        }
+        public IPEndPoint server_end_point
+        {
+            get { return socket.LocalEndPoint as IPEndPoint; }
+        }
+
+        #region ' Login '
+
+        /// <summary>
+        /// If login is failed, then set the value to 0.
+        /// </summary>
+        private UInt32 _login_id { get; set; }
+        /// <summary>
+        /// If login is failed, then set the value to 0.
+        /// </summary>
+        public UInt32 login_id { get { return _login_id; } }
+
+        #endregion
+
+        #endregion
+
+        #region ' Methods '
+
+        #region ' Login '
+
+        public Int32 log_in(String email_address, String password)
+        {
+            using (MySqlConnection connection =
+                new MySqlConnection(CONNECTION_STRING))
+            {
+                using (MySqlCommand command = new MySqlCommand(
+                    "procedure_log_in", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    #region ' Add Parameters '
+
+                    MySqlParameter _email_address = new MySqlParameter(
+                        "_email_address", MySqlDbType.VarString, 254)
+                    {
+                        Value = email_address,
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _password = new MySqlParameter(
+                        "_unencrypted_password", MySqlDbType.VarString, 256)
+                    {
+                        Value = password,
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _server_hostname = new MySqlParameter(
+                        "_server_hostname", MySqlDbType.String, 15)
+                    {
+                        Value = server_end_point.Address.ToString(),
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _server_port = new MySqlParameter(
+                        "_server_port", MySqlDbType.UInt16)
+                    {
+                        Value = server_end_point.Port,
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _user_hostname = new MySqlParameter(
+                        "_user_hostname", MySqlDbType.String, 15)
+                    {
+                        Value = client_end_point.Address.ToString(),
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _user_port = new MySqlParameter(
+                        "_user_port", MySqlDbType.UInt16)
+                    {
+                        Value = client_end_point.Port,
+                        Direction = ParameterDirection.Input
+                    };
+                    MySqlParameter _status_code = new MySqlParameter(
+                        "_status_code", MySqlDbType.UByte)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    MySqlParameter _login_id = new MySqlParameter(
+                        "_login_id", MySqlDbType.UInt32)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+
+                    command.Parameters.Add(_email_address);
+                    command.Parameters.Add(_password);
+                    command.Parameters.Add(_server_hostname);
+                    command.Parameters.Add(_server_port);
+                    command.Parameters.Add(_user_hostname);
+                    command.Parameters.Add(_user_port);
+                    command.Parameters.Add(_status_code);
+                    command.Parameters.Add(_login_id);
+
+                    #endregion
+
+                    #region ' Execute '
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    UInt16 status_code = UInt16.Parse(
+                        _status_code.Value.ToString());
+                    switch (status_code)
+                    {
+                        default:
+                            return status_code;
+                        case 0:
+                            this._login_id = UInt32.Parse(
+                                _login_id.Value.ToString());
+                            return 0;
+                    }
+
+                    #endregion
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
