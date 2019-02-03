eec39f86bfc7f530fd1dd19faa04784c9da40444
NotoOotori <345238563@qq.com>
2019-02-03 17:22:21 +0900

    feat(source/database): 修改登录用存储过程, 添加登出用存储过程
    登录用存储过程中新增对用户名不存在的判断

--- source/database/test.sql
+++ source/database/test.sql
@@ -13,8 +13,10 @@ CALL procedure_sign_up('345238563@qq.com', 'noto_ootori', '123pzy', 'f', '1999-0
 DELETE FROM platform_user
     WHERE username = 'noto_ootori';
 
-CALL procedure_log_in('345238563@qq.com', '123pzy1', '45.32.82.133', 21567, '111.187.30.131', 49137, @_status_code, @_login_id);
+CALL procedure_log_in('345238563@qq.com', '123pzy', '45.32.82.133', 21567, '111.187.30.131', 49137, @_status_code, @_login_id);
 SELECT @_status_code, @_login_id;
+CALL procedure_log_out('345238563@qq.com', 1);
 
 DELETE FROM user_login_record WHERE email_address = '345238563@qq.com';
 ALTER TABLE user_login_record AUTO_INCREMENT = 1;
+DELETE FROM user_logout_record WHERE login_id > 0;
