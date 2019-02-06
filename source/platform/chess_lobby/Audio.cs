a798d52d835b6c17a490928415f678b345315d1c
NotoOotori <345238563@qq.com>
2019-02-07 00:50:52 +0900

    feat(platform.chess_lobby): 添加Audio类的连续播放功能, 更改吃的音效, 更新MySql.Data.
    给Audio类重载了一个play功能, 接受一列字符串, 顺序播放字符串所对应的音频直至全部播放完一遍.

--- source/platform/chess_lobby/Audio.cs
+++ source/platform/chess_lobby/Audio.cs
@@ -2,19 +2,76 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace platform.chess_lobby
 {
-    class Audio
+    public class Audio
     {
         public static void play(String name)
         {
+            WaveOutEvent output_device = new WaveOutEvent();
             Stream stream = Properties.Resources.ResourceManager.GetStream(name);
-            SoundPlayer player = new SoundPlayer(stream);
-            player.Play();
+            WaveFileReader reader = new WaveFileReader(stream);
+            output_device.Init(reader);
+            output_device.Play();
         }
+
+        public static void play(IEnumerable<String> names)
+        {
+            try
+            {
+                play(names.Single());
+            }
+            catch(InvalidOperationException)
+            {
+                new AudioPlayList().play(names);
+            }
+        }
+    }
+
+    class AudioPlayList
+    {
+        #region ' Constructors '
+
+        public AudioPlayList()
+        {; }
+
+        #endregion
+
+        #region ' Properties '
+
+        private WaveOutEvent output_device { get; set; }
+        private IEnumerator<WaveFileReader> enumerator { get; set; }
+
+        #endregion
+
+        #region ' Methods '
+
+        public void play(IEnumerable<String> names)
+        {
+            output_device = new WaveOutEvent();
+            output_device.PlaybackStopped += on_playback_stopped;
+            IEnumerable<WaveFileReader> readers = new List<WaveFileReader>();
+            foreach(String name in names)
+            {
+                Stream stream = Properties.Resources.ResourceManager.
+                    GetStream(name);
+                WaveFileReader reader = new WaveFileReader(stream);
+                readers = readers.Append(reader);
+            }
+            enumerator = readers.GetEnumerator();
+            on_playback_stopped(new Object(), new EventArgs());
+        }
+
+        private void on_playback_stopped(Object sender, EventArgs e)
+        {
+            if (!enumerator.MoveNext())
+                return;
+            output_device.Init(enumerator.Current);
+            output_device.Play();
+        }
+
+        #endregion
     }
 }
