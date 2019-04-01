b49638b46a0555a6662561d4dd849e2a38c8d789
NotoOotori <345238563@qq.com>
2019-04-01 21:15:14 +0900

    fix(platform.chess_lobby): Restyle the form.

--- source/platform/chess_lobby/chess_lobby.Designer.cs
+++ source/platform/chess_lobby/chess_lobby.Designer.cs
@@ -28,51 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.chessboard_container = new platform.chess_lobby.ChessboardContainer();
-            this.button_ready = new platform.GlossyButton();
-            this.button_draw = new platform.GlossyButton();
-            this.button_surrender = new platform.GlossyButton();
             this.label_ready = new System.Windows.Forms.Label();
+            this.button_surrender = new platform.GlossyButton();
+            this.button_draw = new platform.GlossyButton();
+            this.button_ready = new platform.GlossyButton();
+            this.chessboard_container = new platform.chess_lobby.ChessboardContainer();
             this.SuspendLayout();
             // 
-            // chessboard_container
+            // label_ready
             // 
-            this.chessboard_container.Location = new System.Drawing.Point(555, 60);
-            this.chessboard_container.Name = "chessboard_container";
-            this.chessboard_container.Size = new System.Drawing.Size(810, 900);
-            this.chessboard_container.TabIndex = 0;
-            // 
-            // button_ready
-            // 
-            this.button_ready.BackColor = System.Drawing.Color.Orange;
-            this.button_ready.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button_ready.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.button_ready.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.button_ready.DownColor = System.Drawing.Color.Blue;
-            this.button_ready.EnterColor = System.Drawing.Color.Pink;
-            this.button_ready.Location = new System.Drawing.Point(1450, 580);
-            this.button_ready.Margin = new System.Windows.Forms.Padding(2);
-            this.button_ready.Name = "button_ready";
-            this.button_ready.NormalColor = System.Drawing.Color.Orange;
-            this.button_ready.Size = new System.Drawing.Size(100, 50);
-            this.button_ready.TabIndex = 1;
-            this.button_ready.Click += new System.EventHandler(this.button_ready_Click);
-            // 
-            // button_draw
-            // 
-            this.button_draw.BackColor = System.Drawing.Color.Orange;
-            this.button_draw.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button_draw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.button_draw.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.button_draw.DownColor = System.Drawing.Color.Blue;
-            this.button_draw.EnterColor = System.Drawing.Color.Pink;
-            this.button_draw.Location = new System.Drawing.Point(1450, 680);
-            this.button_draw.Margin = new System.Windows.Forms.Padding(2);
-            this.button_draw.Name = "button_draw";
-            this.button_draw.NormalColor = System.Drawing.Color.Orange;
-            this.button_draw.Size = new System.Drawing.Size(100, 50);
-            this.button_draw.TabIndex = 2;
-            this.button_draw.Click += new System.EventHandler(this.button_draw_Click);
+            this.label_ready.AutoSize = true;
+            this.label_ready.BackColor = System.Drawing.Color.Transparent;
+            this.label_ready.Font = new System.Drawing.Font("FZShuTi", 50F);
+            this.label_ready.ForeColor = System.Drawing.Color.Cyan;
+            this.label_ready.Location = new System.Drawing.Point(80, 570);
+            this.label_ready.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_ready.Name = "label_ready";
+            this.label_ready.Size = new System.Drawing.Size(298, 70);
+            this.label_ready.TabIndex = 5;
+            this.label_ready.Text = "准备就绪";
+            this.label_ready.Visible = false;
             // 
             // button_surrender
             // 
@@ -82,7 +57,7 @@
             this.button_surrender.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button_surrender.DownColor = System.Drawing.Color.Blue;
             this.button_surrender.EnterColor = System.Drawing.Color.Pink;
-            this.button_surrender.Location = new System.Drawing.Point(1450, 780);
+            this.button_surrender.Location = new System.Drawing.Point(1275, 720);
             this.button_surrender.Margin = new System.Windows.Forms.Padding(2);
             this.button_surrender.Name = "button_surrender";
             this.button_surrender.NormalColor = System.Drawing.Color.Orange;
@@ -90,26 +65,51 @@
             this.button_surrender.TabIndex = 3;
             this.button_surrender.Click += new System.EventHandler(this.button_surrender_Click);
             // 
-            // label_ready
+            // button_draw
             // 
-            this.label_ready.AutoSize = true;
-            this.label_ready.BackColor = System.Drawing.Color.Transparent;
-            this.label_ready.Font = new System.Drawing.Font("FZShuTi", 75F);
-            this.label_ready.ForeColor = System.Drawing.Color.Cyan;
-            this.label_ready.Location = new System.Drawing.Point(80, 675);
-            this.label_ready.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label_ready.Name = "label_ready";
-            this.label_ready.Size = new System.Drawing.Size(445, 105);
-            this.label_ready.TabIndex = 5;
-            this.label_ready.Text = "准备就绪";
-            this.label_ready.Visible = false;
+            this.button_draw.BackColor = System.Drawing.Color.Orange;
+            this.button_draw.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_draw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.button_draw.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button_draw.DownColor = System.Drawing.Color.Blue;
+            this.button_draw.EnterColor = System.Drawing.Color.Pink;
+            this.button_draw.Location = new System.Drawing.Point(1275, 620);
+            this.button_draw.Margin = new System.Windows.Forms.Padding(2);
+            this.button_draw.Name = "button_draw";
+            this.button_draw.NormalColor = System.Drawing.Color.Orange;
+            this.button_draw.Size = new System.Drawing.Size(100, 50);
+            this.button_draw.TabIndex = 2;
+            this.button_draw.Click += new System.EventHandler(this.button_draw_Click);
+            // 
+            // button_ready
+            // 
+            this.button_ready.BackColor = System.Drawing.Color.Orange;
+            this.button_ready.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_ready.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.button_ready.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button_ready.DownColor = System.Drawing.Color.Blue;
+            this.button_ready.EnterColor = System.Drawing.Color.Pink;
+            this.button_ready.Location = new System.Drawing.Point(1275, 520);
+            this.button_ready.Margin = new System.Windows.Forms.Padding(2);
+            this.button_ready.Name = "button_ready";
+            this.button_ready.NormalColor = System.Drawing.Color.Orange;
+            this.button_ready.Size = new System.Drawing.Size(100, 50);
+            this.button_ready.TabIndex = 1;
+            this.button_ready.Click += new System.EventHandler(this.button_ready_Click);
+            // 
+            // chessboard_container
+            // 
+            this.chessboard_container.Location = new System.Drawing.Point(418, 25);
+            this.chessboard_container.Name = "chessboard_container";
+            this.chessboard_container.Size = new System.Drawing.Size(720, 800);
+            this.chessboard_container.TabIndex = 0;
             // 
             // ChessLobby
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1540, 845);
             this.Controls.Add(this.label_ready);
             this.Controls.Add(this.button_surrender);
             this.Controls.Add(this.button_draw);
