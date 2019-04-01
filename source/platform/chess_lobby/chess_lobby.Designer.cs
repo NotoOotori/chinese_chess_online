24287a09b2c73919731152099a716b4766f216f2
NotoOotori <345238563@qq.com>
2019-04-01 20:25:04 +0900

    feat(platform.chess_lobby): Add label_ready.

--- source/platform/chess_lobby/chess_lobby.Designer.cs
+++ source/platform/chess_lobby/chess_lobby.Designer.cs
@@ -32,6 +32,7 @@
             this.button_ready = new platform.GlossyButton();
             this.button_draw = new platform.GlossyButton();
             this.button_surrender = new platform.GlossyButton();
+            this.label_ready = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // chessboard_container
@@ -49,7 +50,7 @@
             this.button_ready.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button_ready.DownColor = System.Drawing.Color.Blue;
             this.button_ready.EnterColor = System.Drawing.Color.Pink;
-            this.button_ready.Location = new System.Drawing.Point(600, 980);
+            this.button_ready.Location = new System.Drawing.Point(1450, 580);
             this.button_ready.Margin = new System.Windows.Forms.Padding(2);
             this.button_ready.Name = "button_ready";
             this.button_ready.NormalColor = System.Drawing.Color.Orange;
@@ -65,7 +66,7 @@
             this.button_draw.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button_draw.DownColor = System.Drawing.Color.Blue;
             this.button_draw.EnterColor = System.Drawing.Color.Pink;
-            this.button_draw.Location = new System.Drawing.Point(750, 980);
+            this.button_draw.Location = new System.Drawing.Point(1450, 680);
             this.button_draw.Margin = new System.Windows.Forms.Padding(2);
             this.button_draw.Name = "button_draw";
             this.button_draw.NormalColor = System.Drawing.Color.Orange;
@@ -81,7 +82,7 @@
             this.button_surrender.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button_surrender.DownColor = System.Drawing.Color.Blue;
             this.button_surrender.EnterColor = System.Drawing.Color.Pink;
-            this.button_surrender.Location = new System.Drawing.Point(900, 980);
+            this.button_surrender.Location = new System.Drawing.Point(1450, 780);
             this.button_surrender.Margin = new System.Windows.Forms.Padding(2);
             this.button_surrender.Name = "button_surrender";
             this.button_surrender.NormalColor = System.Drawing.Color.Orange;
@@ -89,12 +90,27 @@
             this.button_surrender.TabIndex = 3;
             this.button_surrender.Click += new System.EventHandler(this.button_surrender_Click);
             // 
+            // label_ready
+            // 
+            this.label_ready.AutoSize = true;
+            this.label_ready.BackColor = System.Drawing.Color.Transparent;
+            this.label_ready.Font = new System.Drawing.Font("FZShuTi", 75F);
+            this.label_ready.ForeColor = System.Drawing.Color.Cyan;
+            this.label_ready.Location = new System.Drawing.Point(80, 675);
+            this.label_ready.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_ready.Name = "label_ready";
+            this.label_ready.Size = new System.Drawing.Size(445, 105);
+            this.label_ready.TabIndex = 5;
+            this.label_ready.Text = "准备就绪";
+            this.label_ready.Visible = false;
+            // 
             // ChessLobby
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label_ready);
             this.Controls.Add(this.button_surrender);
             this.Controls.Add(this.button_draw);
             this.Controls.Add(this.button_ready);
@@ -103,6 +119,7 @@
             this.Text = "chess_lobby";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -112,5 +129,6 @@
         private GlossyButton button_ready;
         private GlossyButton button_draw;
         private GlossyButton button_surrender;
+        private System.Windows.Forms.Label label_ready;
     }
 }
